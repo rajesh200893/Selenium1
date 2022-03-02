e0014c546797dc5e912be20cd9d65fcd38433e77
puja <puja@Ravi>
2022-03-02 15:37:00 +0530

    key

--- src/Demo.java
+++ src/Demo.java
@@ -3,13 +3,10 @@ public class Demo
 {public static void main(String[] args) {
 	System.out.println("hi..from local");
 	//hi from github
-<<<<<<< HEAD
 	//hi
-=======
 	//hi
 	//hiiiiiiiiiii
 	
->>>>>>> branch 'master' of https://github.com/rajesh200893/selenium1.git
 }
 
 }
